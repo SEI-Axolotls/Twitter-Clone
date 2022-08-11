e562c3c7671653a468db7a330b5f4b7133c0fdf4
Womack <D_wom3@yahoo.com>
2022-08-11 19:42:09 -0400

    created spitter_app and user_auth. fixed settings in spitter settings.py. migrated. create first sample models. added POST and Comment to admin.py.

---
+++ Back-End/Spitter/spitter_app/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class SpitterAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'spitter_app'
---
+++ Back-End/Spitter/spitter_app/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+# Create your models here.
+class Post(models.Model):
+    author = models.CharField(max_length=120)
+    title = models.CharField(max_length=50)
+    body = models.CharField(max_length=500)
+
+
+    def __str__(self):
+        return self.author
+
+class Comment(models.Model):
+    author = models.CharField(max_length=25)
+    body = models.CharField(max_length=255)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='posts')
+
+    def __str__(self):
+        return self.author
