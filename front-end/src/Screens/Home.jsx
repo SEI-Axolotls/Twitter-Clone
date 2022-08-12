2eba5852e5e2789ced6924e40fd7cf2ff1d57bf5
Christine Manna <cimanna@gmail.com>
2022-08-12 13:08:51 -0400

    nav component added to MyProfile

--- front-end/src/Screens/Home.jsx
+++ front-end/src/Screens/Home.jsx
@@ -1,1 +1,2 @@
 import Nav from '../Components/Nav';
+
--- front-end/src/Screens/MyProfile.jsx
+++ front-end/src/Screens/MyProfile.jsx
@@ -1,1 +1,17 @@
 import Nav from '../Components/Nav';
+
+export default function MyProfile() {
+  return (
+    <div className='my-profile'>
+      <Nav />
+      <div className='my-profile-container'>
+        
+              <h2>Name:</h2>
+              <h2>Email:</h2>
+              <h2>Password:</h2>
+              <h2>Bio:</h2>
+            </div>
+         
+    </div>
+  )
+}
