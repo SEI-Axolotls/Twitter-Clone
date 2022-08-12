3cf3629b06a45d045c363243ba611c1b18be9a5c
Carlton James Jr <cjames222@Genayas-MacBook-Air.local>
2022-08-12 11:23:52 -0400

    Created user profile model and user_auth serializers

--- Back-End/Spitter/spitter_app/models.py
+++ Back-End/Spitter/spitter_app/models.py
@@ -1,6 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class User_profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, default=4)
+    name = models.CharField(max_length=50)
+    email = models.CharField(max_length=50)
+    bio = models.CharField(max_length=150)
+    profile_pic_url = models.TextField()
+
+    def __str__(self):
+        return self.name
+
 class Post(models.Model):
     author = models.CharField(max_length=25)
     title = models.CharField(max_length=50)
