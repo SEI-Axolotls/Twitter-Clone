9f67bb76cfc5e0eca1c7e2f0c889ede8b1795531
Mitchell Raznick <mraznick@Mitchells-MacBook-Air.local>
2022-08-12 14:41:01 -0400

    built serializers in spitter_app

--- Back-End/Spitter/spitter_app/models.py
+++ Back-End/Spitter/spitter_app/models.py
@@ -2,6 +2,8 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
+
+
 class User_profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, default=4)
     name = models.CharField(max_length=50)
@@ -12,19 +14,21 @@ class User_profile(models.Model):
     def __str__(self):
         return self.name
 
+
 class Post(models.Model):
     user = models.ForeignKey(User_profile, on_delete=models.CASCADE)
     title = models.CharField(max_length=50)
     body = models.CharField(max_length=500)
 
-
     def __str__(self):
         return self.author
+
 
 class Comment(models.Model):
     user = models.ForeignKey(User_profile, on_delete=models.CASCADE)
     body = models.CharField(max_length=255)
-    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='posts')
+    post = models.ForeignKey(
+        Post, on_delete=models.CASCADE, related_name='posts')
 
     def __str__(self):
         return self.author
