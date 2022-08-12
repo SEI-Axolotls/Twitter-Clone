f21fd240a3363493ed3caa6d222d3ce8d60175a1
Christine Manna <cimanna@gmail.com>
2022-08-12 12:19:38 -0400

    updated App imports

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -2,10 +2,10 @@ import { Routes, Route } from 'react-router-dom';
 import Home from './Screens/Home';
 import Signin from './Screens/Signin';
 import Signup from './Screens/Signup';
-import Profile from './Screens/Profile';
+import MyProfile from './Screens/MyProfile';
 import Post from './Screens/Post';
-import CommentDetail from './Screens/CommentDetail';
-import Friend from './Screens/Friend';
+import Detail from './Screens/Detail';
+import User from './Screens/User';
 import './App.css';
 
 export default function App() {
