c396c6e0570be6e9411fa0ad0e80092e00e17e36
Christine Manna <cimanna@gmail.com>
2022-08-12 13:03:17 -0400

    nav component added to Detail page

--- front-end/src/Screens/Detail.jsx
+++ front-end/src/Screens/Detail.jsx
@@ -1,1 +1,12 @@
 import Nav from '../Components/Nav';
+
+export default function Detail() {
+  return (
+    <div className='details'>
+      <Nav />
+      <div className='detail-container'>
+      </div>
+
+      </div>
+)
+}
