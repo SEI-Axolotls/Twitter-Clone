d7ba0a0c04de83b0acb6f29fdd5cc4f7cd39e7b8
Carlton James Jr <cjames222@Genayas-MacBook-Air.local>
2022-08-12 13:45:44 -0400

    Registered User_profile

--- Back-End/Spitter/spitter_app/admin.py
+++ Back-End/Spitter/spitter_app/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Post, Comment
+from .models import Post, Comment, User_profile
 
 # Register your models here.
+admin.site.register(User_profile)
 admin.site.register(Post)
 admin.site.register(Comment)
