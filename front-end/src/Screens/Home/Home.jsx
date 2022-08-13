b5b17347a476289a05403f2f598b34fa95f261bd
Timothy Tang <timothytang@Timothys-MacBook-Pro.local>
2022-08-13 11:09:30 -0400

    Logout component and dummy posts inserted

--- front-end/src/Screens/Home/Home.jsx
+++ front-end/src/Screens/Home/Home.jsx
@@ -1,10 +1,26 @@
 import Layout from '../../Components/Layout';
+import {postsData} from './postsData.js'
+import { useState, useEffect } from 'react';
 
 export default function Home({ user}) {
+  const [posts, setPosts] = useState([])
+
+  useEffect(() => {
+    setPosts(postsData)
+
+  },[])
+
+
   return (
     <Layout user={user}>
     <div className='home'>
       <h1>Hi</h1>
+      {posts.map((post)=> (
+        <div>
+          <h3> {post.title}</h3>
+        <p>{post.body}</p>
+        </div>
+      ))}
       </div>
     </Layout>
   )
---
+++ front-end/src/Screens/Home/postsData.js
@@ -0,0 +1,42 @@
+ export const postsData = [
+    {
+      title: "Looking for a change of scenary?",
+      body: "I am of the mind to move out of the US and start fresh! Anyone recommend a good spot?",
+      comments: [
+        "Move to Portugal!",
+        "Portugal is overrated - move to Iceland instead!",
+      ],
+    },
+    {
+      title: "Pottery",
+      body: "If you don't make pottery with natural clay, you are a newb. Change my mind.",
+      comments: [
+        "What? Natural clay has too many imperfections.",
+        "Not easy, but gotta pound out the imperfections first",
+      ],
+    },
+    {
+      title: "Murals - what are they good for?",
+      body: "I see a bunch of murals going up in my neighborhood - who has a say in what is put up?",
+      comments: [
+        "Talk to your city officials if you want a say",
+        "I love the murals! Adds so much vibrancy to our area!",
+      ],
+    },
+    {
+      title: "Couch Surfing",
+      body: "The best place to couch surf is in California! Everyone is so friendly.",
+      comments: [
+        "Are you crazy!?! Cali does not have the nicest people",
+        "Try Colorado instead - we love having people over",
+      ],
+    },
+    {
+      title: "Meatballs - a back story",
+      body: "Meatballs are generally made with the worst cuts of an animal. My suggestion, go vegan instead. You get the best of the plants!",
+      comments: [
+        "Pass",
+        "What kind of plants will you use for this - like a mushroom of some sort?",
+      ],
+    },
+  ];
