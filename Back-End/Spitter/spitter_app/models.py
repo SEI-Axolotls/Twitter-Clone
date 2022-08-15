392c84d40693f32ea663257c93d969d127409ed2
Carlton James Jr <cjames222@Genayas-MacBook-Air.local>
2022-08-15 14:15:11 -0400

    Fixed the comments issue

--- Back-End/Spitter/spitter_app/models.py
+++ Back-End/Spitter/spitter_app/models.py
@@ -5,12 +5,12 @@ from django.contrib.auth.models import User
 
 
 class User_profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE, default=4)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, default=1)
     name = models.CharField(max_length=50)
     email = models.CharField(max_length=50)
-    bio = models.CharField(max_length=150)
-    profile_pic_url = models.TextField()
-
+    profile_pic_url = models.CharField(max_length=150)
+    bio = models.TextField()
+    
     def __str__(self):
         return self.name
 
@@ -27,8 +27,7 @@ class Post(models.Model):
 class Comment(models.Model):
     user = models.ForeignKey(User_profile, on_delete=models.CASCADE)
     body = models.CharField(max_length=255)
-    post = models.ForeignKey(
-        Post, on_delete=models.CASCADE, related_name='posts')
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.body
