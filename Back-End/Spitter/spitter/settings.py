c271ce35940d06d05b4498bc7d37c8d95ea5f91a
Carlton James Jr <cjames222@Genayas-MacBook-Air.local>
2022-08-16 15:30:50 -0400

    Edited settings

--- Back-End/Spitter/spitter/settings.py
+++ Back-End/Spitter/spitter/settings.py
@@ -160,8 +160,6 @@ WHITENOISE_USE_FINDERS = True
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles'),
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static')
